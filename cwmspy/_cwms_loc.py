cf9ed966cc8f790b870ac72618f8d00f481bbce9
Jeff Tilton <jfftilton@gmail.com>
2019-08-16 22:10:32 -0700

    docstring test
    trying to get numpy docstrings to work with pdoc3

--- cwmspy/_cwms_loc.py
+++ cwmspy/_cwms_loc.py
@@ -3,6 +3,41 @@
 
 from cx_Oracle import DatabaseError
 class CWMS_LOC:
+    def geodetic2aer(lat: float, lon: float, h: float,
+                 lat0: float, lon0: float, h0: float,
+                 ell: Ellipsoid = None, deg: bool = True) -> Tuple[float, float, float]:
+    """
+    gives azimuth, elevation and slant range from an Observer to a Point with geodetic coordinates.
+    Parameters
+    ----------
+    lat : float or numpy.ndarray of float
+        target geodetic latitude
+    lon : float or numpy.ndarray of float
+        target geodetic longitude
+    h : float or numpy.ndarray of float
+        target altitude above geodetic ellipsoid (meters)
+    lat0 : float
+        Observer geodetic latitude
+    lon0 : float
+        Observer geodetic longitude
+    h0 : float
+         observer altitude above geodetic ellipsoid (meters)
+    ell : Ellipsoid, optional
+          reference ellipsoid
+    deg : bool, optional
+          degrees input/output  (False: radians in/out)
+    Returns
+    -------
+    az : float or numpy.ndarray of float
+         azimuth
+    el : float or numpy.ndarray of float
+         elevation
+    srange : float or numpy.ndarray of float
+         slant range [meters]
+    """
+    e, n, u = geodetic2enu(lat, lon, h, lat0, lon0, h0, ell, deg=deg)
+
+    return enu2aer(e, n, u, deg=deg)
 
     def store_location(self, p_location_id, p_location_type=None,
                        p_elevation=None, p_elev_unit_id=None,
@@ -12,57 +47,57 @@ class CWMS_LOC:
                        p_time_zone_id=None, p_country_name=None,
                        p_state_initial=None, p_active=None, p_ignorenulls='T',
                        p_db_office_id=None):
-        """
-        Short summary.
+    """
+    Short summary.
 
-        Parameters
-        ----------
-        
-        p_location_id : str
-            The location identifier.
-        p_location_type : str
-            A user-defined type for the location.
-        p_elevation : float
-            The elevation of the location.
-        p_elev_unit_id : str
-            The elevation unit.
-        p_vertical_datum : str
-            The datum of the elevation.
-        p_latitude : float
-            The actual latitude of the location.
-        p_longitude : float
-            The actual longitude of the location.
-        p_horizontal_datum : str
-            The datum for the latitude and longitude.
-        p_public_name : str
-            The public name for the location.
-        p_long_name : str
-            The long name for the location.
-        p_description : str
-            A description of the location.
-        p_time_zone_id : str
-            The time zone name for the location.
-        p_country_name : str
-            The name of the county that the location is in.
-        p_state_initial : str
-            The two letter abbreviation of the state that the location is in.
-        p_active : str
-             flag ('T' or 'F') that specifies whether the location is marked 
-             as active.
-        p_ignorenulls : str
-            A flag ('T' or 'F') that specifies whether to ignore NULL 
-            parameters. If 'F', existing data will be updated with NULL 
-            parameter values.
-        p_db_office_id : str
-            The office that owns the location. If not specified or NULL, the 
-            session user's default office will be used.
+    Parameters
+    ----------
 
-        Returns
-        -------
-        Boolean
-            True for success.
+    p_location_id : str
+        The location identifier.
+    p_location_type : str
+        A user-defined type for the location.
+    p_elevation : float
+        The elevation of the location.
+    p_elev_unit_id : str
+        The elevation unit.
+    p_vertical_datum : str
+        The datum of the elevation.
+    p_latitude : float
+        The actual latitude of the location.
+    p_longitude : float
+        The actual longitude of the location.
+    p_horizontal_datum : str
+        The datum for the latitude and longitude.
+    p_public_name : str
+        The public name for the location.
+    p_long_name : str
+        The long name for the location.
+    p_description : str
+        A description of the location.
+    p_time_zone_id : str
+        The time zone name for the location.
+    p_country_name : str
+        The name of the county that the location is in.
+    p_state_initial : str
+        The two letter abbreviation of the state that the location is in.
+    p_active : str
+         flag ('T' or 'F') that specifies whether the location is marked 
+         as active.
+    p_ignorenulls : str
+        A flag ('T' or 'F') that specifies whether to ignore NULL 
+        parameters. If 'F', existing data will be updated with NULL 
+        parameter values.
+    p_db_office_id : str
+        The office that owns the location. If not specified or NULL, the 
+        session user's default office will be used.
 
-        """
+    Returns
+    -------
+    Boolean
+        True for success.
+
+    """
         cur = self.conn.cursor()
 
 
