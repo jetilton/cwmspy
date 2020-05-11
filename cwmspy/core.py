ccb459f843831c5e5decc64200d9cf841650dafb
jetilton <jfftilton@gmail.com>
2020-05-11 11:42:25 -0700

    changed environment variables to CWMSPY_var and added dsn
    Former-commit-id: 27b384a08c4a7500afe68c165a4de3d424190f8a

--- cwmspy/core.py
+++ cwmspy/core.py
@@ -2,7 +2,6 @@
 
 import sys
 import cx_Oracle
-from dotenv import load_dotenv
 import os
 from os.path import join, dirname
 import logging
@@ -17,21 +16,24 @@ LOGGER = logging.getLogger(__name__)
 LD = log_decorator(LOGGER)
 FORMAT = "%(levelname)s - %(asctime)s - %(name)s - %(message)s"
 
-DOTENV_PATH = join(dirname(__file__), ".env")
-load_dotenv(DOTENV_PATH)
-
 
 class CWMS(CwmsLocMixin, CwmsTsMixin, CwmsLevelMixin):
     def __init__(self, conn=None, verbose=False):
         self.conn = conn
         if verbose:
-            logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=FORMAT)
+            logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format=FORMAT)
         else:
             logging.basicConfig(stream=sys.stderr, level=logging.ERROR, format=FORMAT)
 
     @LD
     def connect(
-        self, host=None, service_name=None, port=1521, user=None, password=None
+        self,
+        host=None,
+        service_name=None,
+        port=1521,
+        user=None,
+        password=None,
+        dsn=None,
     ):
         """Make connection to Oracle CWMS database. Oracle connections are
             expensive, so it is best to have a class connection for all methods.
@@ -87,54 +89,50 @@ class CWMS(CwmsLocMixin, CwmsTsMixin, CwmsLevelMixin):
         ```
 
         """
-
-        dsn_dict = {}
-        if host:
-            dsn_dict.update({"host": host})
-        elif os.getenv("HOST"):
-            host = os.getenv("HOST")
-            dsn_dict.update({"host": host})
-        else:
-            msg = "Missing host"
-            LOGGER.error(msg)
-            raise ValueError("Missing host")
-        LOGGER.info(f"Host: {host}")
-        if service_name:
-            dsn_dict.update({"service_name": service_name})
-        elif os.getenv("SERVICE_NAME"):
-            service_name = os.getenv("SERVICE_NAME")
-            dsn_dict.update({"service_name": os.getenv("SERVICE_NAME")})
-        else:
-            msg = "Missing service_name"
-            LOGGER.error(msg)
-            raise ValueError(msg)
-        LOGGER.info(f"service_name: {service_name}")
-        if port:
-            dsn_dict.update({"port": port})
-        elif os.getenv("PORT"):
-            dsn_dict.update({"port": os.getenv("PORT")})
-        else:
-            msg = "Missing port"
-            LOGGER.error(msg)
-            raise ValueError(msg)
-        LOGGER.info(f"port: {port}")
-        self.host = dsn_dict["host"]
-        dsn = cx_Oracle.makedsn(**dsn_dict)
+        if not dsn:
+            dsn_dict = {}
+            if host:
+                dsn_dict.update({"host": host})
+            elif os.getenv("CWMSPY_HOST"):
+                host = os.getenv("CWMSPY_HOST")
+                dsn_dict.update({"host": host})
+            else:
+                msg = "Missing host"
+                LOGGER.error(msg)
+                raise ValueError("Missing host")
+            LOGGER.info(f"Host: {host}")
+            if service_name:
+                dsn_dict.update({"service_name": service_name})
+            elif os.getenv("CWMSPY_SERVICE_NAME"):
+                service_name = os.getenv("CWMSPY_SERVICE_NAME")
+                dsn_dict.update({"service_name": os.getenv("CWMSPY_SERVICE_NAME")})
+            else:
+                msg = "Missing service_name"
+                LOGGER.error(msg)
+                raise ValueError(msg)
+            LOGGER.info(f"service_name: {service_name}")
+            if port:
+                dsn_dict.update({"port": port})
+            elif os.getenv("CWMSPY_PORT"):
+                dsn_dict.update({"port": os.getenv("CWMSPY_PORT")})
+            else:
+                msg = "Missing port"
+                LOGGER.error(msg)
+                raise ValueError(msg)
+            LOGGER.info(f"port: {port}")
+            self.host = dsn_dict["host"]
+            dsn = cx_Oracle.makedsn(**dsn_dict)
 
         conn_dict = {"dsn": dsn}
 
         if user:
             conn_dict.update({"user": user})
-        elif os.getenv("USER"):
-            conn_dict.update({"user": os.getenv("USER")})
-        else:
-            conn_dict.update({"user": "cwmsview"})
+        elif os.getenv("CWMSPY_USER"):
+            conn_dict.update({"user": os.getenv("CWMSPY_USER")})
         if password:
             conn_dict.update({"password": password})
-        elif os.getenv("PASSWORD"):
-            conn_dict.update({"password": os.getenv("PASSWORD")})
-        else:
-            conn_dict.update({"password": "cwmsview"})
+        elif os.getenv("CWMSPY_PASSWORD"):
+            conn_dict.update({"password": os.getenv("CWMSPY_PASSWORD")})
 
         # close any current open connection to minimize # of connections to DB
         if self.conn:
