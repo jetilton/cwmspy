69504e367ed3b870d27e1a69d620c5feb709e8eb
jetilton <jfftilton@gmail.com>
2020-09-22 08:21:03 -0700

    updated retrieve location to have more metadata

--- cwmspy/cwms_loc.py
+++ cwmspy/cwms_loc.py
@@ -4,7 +4,8 @@ Facilities for working with locations in the CWMS database
 """
 import logging
 from .utils import log_decorator
-
+import cx_Oracle
+import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
 LD = log_decorator(LOGGER)
@@ -138,7 +139,7 @@ class CwmsLocMixin:
         p_delete_action : str
 
             Specifies what to delete. Actions are as follows:
-                
+
             - <span style="color:#bf2419">`"DELETE LOC"`</span>
             -- deletes only this location, and then only if it has no associated dependent data
             - <span style="color:#bf2419">`"'DELETE KEY"`</span>
@@ -155,7 +156,7 @@ class CwmsLocMixin:
             -- deletes this location and all dependent data, if any
             - <span style="color:#bf2419">`"DELETE ALL"`</span>
             -- deletes this location and all dependent data, if any
-        
+
         p_db_office_id : str
             The office that owns the location. If not specified or NULL, the
             session user's default office will be used.
@@ -196,27 +197,26 @@ class CwmsLocMixin:
         return True
 
     @LD
-    def retrieve_location(self, p_location_id):
-        """
-        [summary]
-
-        [extended_summary]
+    def retrieve_location(
+        self, p_location_id, p_elev_unit_id="m", p_db_office_id=None, return_df=True
+    ):
+        """Retreives location information from the database.
 
         Parameters
         ----------
         p_location_id : str
             The location identifier.
+        p_elev_unit_id : str
+            The unit to retrieve the elevation in (the default is "m").
+        p_db_office_id : str
+            The office that owns the location. If not specified or None, the session user's default office will be used (the default is None).
+        return_df : bool
+            Return a pandas dataframe (the default is True).
 
         Returns
         -------
-        [type]
-            [description]
-
-        Raises
-        ------
-        DatabaseError
-            [description]
-
+        type
+            Pandas dataframe or list of dictionaries
 
         Examples
         -------
@@ -225,27 +225,144 @@ class CwmsLocMixin:
         cwms = CWMS()
         cwms.connect()
 
-        cwms.retrieve_location("TST")
-        [(76140126, 26, 'TST', 'T')]
+        df = cwms.retrieve_location("TST")
         ```
         """
+
         LOGGER.info("Start retrieve_location")
 
         cur = self.conn.cursor()
+        # The below are out parameters.  You need to pass in out parameters to the
+        # procedure if they are listed of the correct type.
+        p_location_type = cur.var(cx_Oracle.STRING)
+        p_elevation = cur.var(cx_Oracle.NUMBER)
+        p_vertical_datum = cur.var(cx_Oracle.STRING)
+        p_latitude = cur.var(cx_Oracle.NUMBER)
+        p_longitude = cur.var(cx_Oracle.NUMBER)
+        p_horizontal_datum = cur.var(cx_Oracle.STRING)
+        p_public_name = cur.var(cx_Oracle.STRING)
+        p_long_name = cur.var(cx_Oracle.STRING)
+        p_description = cur.var(cx_Oracle.STRING)
+        p_time_zone_id = cur.var(cx_Oracle.STRING)
+        p_county_name = cur.var(cx_Oracle.STRING)
+        p_state_initial = cur.var(cx_Oracle.STRING)
+        p_active = cur.var(cx_Oracle.STRING)
+        p_alias_cursor = cur.var(cx_Oracle.CURSOR)
 
-        sql = """
-            select * from cwms_20.at_base_location
-            where base_location_id = '{}'
-            """.format(
-            p_location_id
-        )
+        # These are all of the out parameters that will be returned
+        out_list = [
+            p_location_id,
+            p_location_type,
+            p_elevation,
+            p_vertical_datum,
+            p_latitude,
+            p_longitude,
+            p_horizontal_datum,
+            p_public_name,
+            p_long_name,
+            p_description,
+            p_time_zone_id,
+            p_county_name,
+            p_state_initial,
+            p_active,
+            p_alias_cursor,
+        ]
 
         try:
-            loc = cur.execute(sql).fetchall()
+            in_list = out_list.copy()
+            in_list.insert(1, p_elev_unit_id)
+            in_list += [p_db_office_id]
+            cur.callproc(
+                "cwms_loc.retrieve_location", in_list,
+            )
         except ValueError as e:
             LOGGER.error("Error in retrieve_location.")
             cur.close()
             raise ValueError(e)
         cur.close()
         LOGGER.info("End retrieve_location")
-        return loc
+        alias = [r for r in p_alias_cursor.getvalue()]
+
+        out_dict = [
+            {
+                "name": "location_id",
+                "value": "",
+                "description": "The location identifier",
+            },
+            {
+                "name": "location_type",
+                "value": "",
+                "description": "A user-defined type for the location",
+            },
+            {
+                "name": "elevation",
+                "value": "",
+                "description": "The elevation of the location",
+            },
+            {
+                "name": "vertical_datum",
+                "value": "",
+                "description": "The datum of the elevation",
+            },
+            {
+                "name": "lat",
+                "value": "",
+                "description": "The actual latitude of the location",
+            },
+            {
+                "name": "long",
+                "value": "",
+                "description": "The actual longitude of the location",
+            },
+            {
+                "name": "horizontal_datum",
+                "value": "",
+                "description": "The datum for the latitude and longitude",
+            },
+            {
+                "name": "public_name",
+                "value": "",
+                "description": "The public name for the location",
+            },
+            {
+                "name": "long_name",
+                "value": "",
+                "description": "The long name for the location",
+            },
+            {
+                "name": "description",
+                "value": "",
+                "description": "A description of the location",
+            },
+            {
+                "name": "timezone",
+                "value": "",
+                "description": "The time zone name for the location",
+            },
+            {
+                "name": "county_name",
+                "value": "",
+                "description": "The name of the county that the location is in",
+            },
+            {
+                "name": "state_initial",
+                "value": "",
+                "description": "The two letter abbreviation of the state that the location is in",
+            },
+            {
+                "name": "location_id",
+                "value": "",
+                "description": "The location identifier",
+            },
+            {"name": "alias", "value": "", "description": "Aliases for the location",},
+        ]
+
+        for i, v in enumerate(out_dict):
+            if i == 0:
+                v["value"] = out_list[i]
+            else:
+                v["value"] = out_list[i].getvalue()
+        out_dict[-1]["value"] = alias
+        if return_df:
+            return pd.DataFrame(out_dict).set_index("name")
+        return out_dict
